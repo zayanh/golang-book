f1b45a9c5fe67427d7b46b0e12f918ecfd143975
Zayan Hafeez <zayan.hafeez@gmail.com>
2019-07-21 15:04:32 -0700

    perimeter exercise in chapter 9

--- chapter9/main.go
+++ chapter9/main.go
@@ -16,6 +16,7 @@ type Rectangle struct {
 // "implements" this interface
 type Shape interface {
 	area() float64
+	perimeter() float64
 }
 
 func distance(x1, y1, x2, y2 float64) float64 {
@@ -33,18 +34,34 @@ func(c *Circle) area() float64 {
 	return math.Pi * c.r*c.r
 }
 
-func (r *Rectangle) area() float64 {
-	l := distance(r.x1, r.y1, r.x1, r.y2)
-	w := distance(r.x1, r.y1, r.x2, r.y1)
-	return l * w
+func (c *Circle) perimeter() float64 {
+	return math.Pi * 2*c.r
 }
 
-func totalArea(shapes ...Shape) float64 {
-	var area float64
+func (r *Rectangle) length() float64 {
+	return distance(r.x1, r.y1, r.x1, r.y2)
+}
+
+func (r* Rectangle) width() float64 {
+	return distance(r.x1, r.y1, r.x2, r.y1)
+}
+
+func (r *Rectangle) area() float64 {
+	return r.length() * r.width()
+}
+
+func (r *Rectangle) perimeter() float64 {
+	return 2*(r.length() + r.width())
+}
+
+func totalArea(shapes ...Shape) (float64, float64) {
+	var area, perimeter float64
 	for _, s := range shapes {
 		area += s.area()
+		fmt.Println("perimeter: ", s.perimeter())
+		perimeter += s.perimeter()
 	}
-	return area
+	return area, perimeter
 }
 
 // interface as a field in another type
@@ -96,7 +113,8 @@ func main() {
 	fmt.Println("circle area: ", c.area())
 	fmt.Println("rectangle area: ", r.area())
 
-	fmt.Println("total area: ", totalArea(&c, &r))
+	ar, pr := totalArea(&c, &r)
+	fmt.Println("total area: ", ar, "\ntotal perimeter: ", pr)
 
 	//shapeSlice := [2]Shape{&c, &r}
 	//m := MultiShape{shapeSlice}
